5154611aa341f22ffb8509093406298c8c6978ec
Peter Aboalim <peter.aboalim@gmail.com>
2020-05-27 02:52:41 +0200

    update App home page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React!
+          Hello React in Docker running on AWS Elastic Beanstalk!
         </p>
         <a
           className="App-link"
